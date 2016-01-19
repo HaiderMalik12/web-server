cae64e33a1a0e6ef6388780806d7d49f8be63907
Haider Malik <haidermalik504@gmail.com>
2016-01-19 12:57:04 -0800

    Initialize Repo

---
+++ server.js
@@ -0,0 +1,48 @@
+/**
+ * Created by HaiderNazir on 1/19/2016.
+ */
+var express=require('express');
+var app=express();
+var PORT=3000;
+
+/*route level middleware
+ First execute this then do / this Operation
+* */
+var middleware={
+    requireAuthentication:function(req,res,next){
+        console.log('Private Rout hit');
+        next();
+    },
+    logger:function(req,res,next){
+        console.log('Request : '+req.method +'  '+req.originalUrl +  '  '+ new Date().toString());
+        next();
+    }
+};
+
+//use this middleware to the whole application routes
+app.use(middleware.logger);
+
+/*--Start Routing ---------------*/
+app.get('/',function(req,res){
+
+   res.send('Hello Express !!!');
+});
+
+/*get the about page*/
+app.get('/about',middleware.requireAuthentication,function(req,res){
+    res.send('from About Us');
+});
+
+/*
+ ---------------------------------------------------------
+ start serving static web pages
+ */
+app.use(express.static(__dirname+'/public'));
+
+
+
+/*-------Listen to the Port ------------*/
+app.listen(PORT,function(){
+    console.log('Express Started on PORT '+PORT +'!!');
+});
+
